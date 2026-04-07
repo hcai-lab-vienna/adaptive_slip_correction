2faf51218c08adf8a59e45ca9a8ac92014086fad
Jean-Baptiste Weibel <jb.weibel@gmail.com>
2026-04-07 15:38:06 +0200

    Extract features

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+import numpy as np
+import pandas as pd
+
 from fomo_utils import (
     DEPLOYMENTS,
     TRAJECTORIES,
     get_trajectory_dir,
     get_transforms,
     get_odom_trajectory,
+    get_robot_cmd,
     get_gt_trajectory,
     get_imu_data,
 )
 from imu_utils import mahony_filter, gravity_from_attitude, augment_odometry_with_imu
-from trajectory_utils import (orientations_from_positions,
-                              sync,
-                              reduce_to_ids,
-                              velocities_from_trajectories)
+from trajectory_utils import (
+    orientations_from_positions,
+    sync,
+    reduce_to_ids,
+    relative_pose_from_trajectories,
+    velocities_from_deltaT
+)
 
 
 if __name__ == "__main__":
@@ -24,40 +31,42 @@ if __name__ == "__main__":
                         help=f"Trajectory name. Valid options are {TRAJECTORIES}")
     args = parser.parse_args()
 
+
+    ###########################################################################
     ### Load data
+    print("Load data")
     trajectory_dir = get_trajectory_dir(deployment=args.deployment, trajectory=args.trajectory)
+
     # Transforms
     transform_manager = get_transforms(trajectory_dir)
+
     # IMUs
-    imu_name = 'xsens'
+    imu_name = 'vectornav'  # 'vectornav'
     accel, gyro, imu_timestamps = get_imu_data(trajectory_dir, imu=imu_name, tm=transform_manager)
+
     # Trajectories
     traj_gt = get_gt_trajectory(trajectory_dir)
-    traj_gt_oriented, gt_headings = orientations_from_positions(traj_gt)
     traj_odom, lin_vel_twist, ang_vel_twist = get_odom_trajectory(trajectory_dir)
-    delta_ts_twist = traj_odom.timestamps[1:] - traj_odom.timestamps[:-1]
+    cmd_df = get_robot_cmd(trajectory_dir, return_df=True)
 
-    # Recover linear and angular velocities from GT trajectory
-    lin_vel_gt, ang_vel_gt, traj_gt_oriented, p_rel_gt, delta_ts = velocities_from_trajectories(traj_gt_oriented)
+    ###########################################################################
+    ### Compute properties
+    # IMUs
+    print("Compute IMU orientations")
+    g_earth = 9.80665
+    imu_quats = mahony_filter(accel, gyro, imu_timestamps, g=g_earth, kp=1.0, ki=0.3)
+    g_body = gravity_from_attitude(imu_quats)
+    accel_grav_compensated = accel + g_earth * g_body
 
-    # Synchronize trajectories
-    ids_gt, ids_odom = sync.matching_time_indices(traj_gt_oriented.timestamps, traj_odom.timestamps, max_diff=0.05)
-    lin_vel_gt, ang_vel_gt = reduce_to_ids(lin_vel_gt, ids_gt), reduce_to_ids(ang_vel_gt, ids_gt)
-    p_rel_gt, delta_ts = reduce_to_ids(p_rel_gt, ids_gt), reduce_to_ids(delta_ts, ids_gt)
-    traj_gt_sync = reduce_to_ids(traj_gt_oriented, ids_gt)
-    traj_odom_sync = reduce_to_ids(traj_odom, ids_odom)
-    lin_vel_twist_sync = lin_vel_twist[ids_odom]
-    ang_vel_twist_sync = ang_vel_twist[ids_odom]
+    # GT trajectory
+    print("Process GT trajectory")
+    delta_ts_gt = traj_gt.timestamps[1:] - traj_gt.timestamps[:-1]
+    p_rel_gt = relative_pose_from_trajectories([traj_gt])[0]
+    vel_gt = np.linalg.norm(np.array(p_rel_gt)[:,:3,3], axis=1) / delta_ts_gt
+    gt_timestamps = traj_gt.timestamps[1:]
 
-    # Process the IMU
-    freq = 200.0
-    imu_quats = mahony_filter(accel, gyro, imu_timestamps, g=9.80665, kp=1.0, ki=0.3)
-    # from ahrs.filters import Mahony
-    # orientation = Mahony(gyr=gyro, acc=accel, frequency=freq)
-
-    # Estimate the gravity vector
-    # g_body = gravity_from_attitude(imu_attitude_qs)
-
+    # Wheel + IMU Odometry
+    print("Process Odometry")
     ids_odom_to_imu, ids_imu_to_odom = sync.matching_time_indices(
         traj_odom.timestamps, imu_timestamps, max_diff=0.01)
     traj_odom_imu = augment_odometry_with_imu(
@@ -65,7 +74,67 @@ if __name__ == "__main__":
         reduce_to_ids(lin_vel_twist, ids_odom_to_imu),
         reduce_to_ids(imu_quats, ids_imu_to_odom)
     )
+    p_rel_odom_imu = relative_pose_from_trajectories([traj_odom_imu])[0]
+    delta_ts_odom_imu = traj_odom_imu.timestamps[1:] - traj_odom_imu.timestamps[:-1]
+    vel_odom_imu = [velocities_from_deltaT(dT, dt) for dT, dt in zip(p_rel_odom_imu, delta_ts_odom_imu)]
+    lin_vel_odom_imu = [vl for vl, va in vel_odom_imu]
+    ang_vel_odom_imu = [va for vl, va in vel_odom_imu]
 
+    ###########################################################################
+    ### Align start-end and interpolate
+    # Odom and Command
+    odom_dt_index = pd.to_datetime(traj_odom_imu.timestamps[1:] * 1e9)
+    cmd_df = cmd_df.reindex(odom_dt_index, method='nearest', tolerance=pd.Timedelta('50ms'))
+    cmd_df = cmd_df.interpolate(method="linear", limit_direction="both")
+    lin_vel_cmd = np.array(cmd_df[['lx', 'ly', 'lz']])
+    ang_vel_cmd = np.array(cmd_df[['ax', 'ay', 'az']])
+
+    # IMUs data
+    accel_grav_compensated_sync = reduce_to_ids(accel_grav_compensated, ids_imu_to_odom)[2:]
+    gyro_sync = reduce_to_ids(gyro, ids_imu_to_odom)[2:]
+    g_body_sync = reduce_to_ids(g_body, ids_imu_to_odom)[2:]
+
+    # GT data
+    gt_df = pd.DataFrame({'ts': pd.to_datetime(1e9 * gt_timestamps), 'vel_gt': vel_gt})
+    gt_df = gt_df.set_index('ts')
+    gt_df = gt_df.reindex(odom_dt_index, method='nearest', tolerance=pd.Timedelta('50ms')).interpolate(method='time')
+
+    features = pd.DataFrame(
+        np.hstack([
+            accel_grav_compensated_sync,
+            gyro_sync,
+            g_body_sync,
+            lin_vel_odom_imu,
+            ang_vel_odom_imu,
+            lin_vel_cmd,
+            ang_vel_cmd,
+        ]),
+        columns= [
+            'lin_acc_imu_x',
+            'lin_acc_imu_y',
+            'lin_acc_imu_z',
+            'ang_vel_imu_x',
+            'ang_vel_imu_y',
+            'ang_vel_imu_z',
+            'grav_x',
+            'grav_y',
+            'grav_z',
+            'lin_vel_odom_x',
+            'lin_vel_odom_y',
+            'lin_vel_odom_z',
+            'ang_vel_odom_x',
+            'ang_vel_odom_y',
+            'ang_vel_odom_z',
+            'lin_vel_cmd_x',
+            'lin_vel_cmd_y',
+            'lin_vel_cmd_z',
+            'ang_vel_cmd_x',
+            'ang_vel_cmd_y',
+            'ang_vel_cmd_z',
+        ]
+    )
+
+    print("Display trajectories")
     import matplotlib.pyplot as plt
     plt.plot(traj_odom_imu.positions_xyz[:,0], traj_odom_imu.positions_xyz[:,1])
     plt.plot(traj_odom.positions_xyz[:,0], traj_odom.positions_xyz[:,1])
